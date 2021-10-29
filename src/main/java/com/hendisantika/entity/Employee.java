ed0cb7920fc4a6199e583d2391768c2750ac00a8
Hendi Santika <hendisantika@gmail.com>
2021-10-30 05:49:20 +0700

    #4 - Add Employee Entity

---
+++ src/main/java/com/hendisantika/entity/Employee.java
@@ -0,0 +1,33 @@
+package com.hendisantika.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 30/10/21
+ * Time: 05.46
+ */
+@Entity
+@NoArgsConstructor
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
