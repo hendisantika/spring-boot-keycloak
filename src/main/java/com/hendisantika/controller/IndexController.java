851cdfa75b0f1db2e06dfb05fb373ae3d056255a
Hendi Santika <hendisantika@gmail.com>
2021-10-30 05:56:11 +0700

    #15 - Add getEmployee in IndexController

--- src/main/java/com/hendisantika/controller/IndexController.java
+++ src/main/java/com/hendisantika/controller/IndexController.java
@@ -1,9 +1,15 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.Employee;
 import com.hendisantika.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +26,10 @@ public class IndexController {
     @Autowired
     private EmployeeService employeeService;
 
+    //this method can be accessed by user whose role is user
+    @GetMapping("/{employeeId}")
+    @RolesAllowed("user")
+    public ResponseEntity<Employee> getEmployee(@PathVariable int employeeId) {
+        return ResponseEntity.ok(employeeService.getEmployee(employeeId));
+    }
 }
